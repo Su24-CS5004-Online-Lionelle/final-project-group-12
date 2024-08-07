d512230974394cc4d1d676d5c0c16a13528f0049
YangChengLuo <156385953+YangChengLuo@users.noreply.github.com>
2024-08-07 01:57:20 -0700

    Update ZooApp.java

--- src/main/java/zoosys/ZooApp.java
+++ src/main/java/zoosys/ZooApp.java
@@ -11,6 +11,8 @@ public class ZooApp {
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 System.out.println("Starting Zoo Management Application...");
+                IVisitor visitor = new Visitor();
+                visitor.readCSV();
 
                 // Initialize the models
                 EmployeeManagement employeeManagement = new EmployeeManagement();
