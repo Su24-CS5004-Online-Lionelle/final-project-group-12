ec14d507dabb0a0fa349ce56ce12454540a0607a
YangChengLuo <156385953+YangChengLuo@users.noreply.github.com>
2024-08-12 12:41:32 -0700

    Update AnimalCategory.java

--- src/main/java/zoosys/model/AnimalCategory.java
+++ src/main/java/zoosys/model/AnimalCategory.java
@@ -1,5 +1,12 @@
 package zoosys.model;
 
+
+/**
+ * Enum that have categories of animals in the zoo system.
+ */
 public enum AnimalCategory {
+    /**
+     * Represents animals that are predator, or herbivore, or primate, or bird, or reptile, or marine animal.
+     */
     PREDATOR, HERBIVORE, PRIMATE, BIRD, REPTILE, MARINE;
 }
