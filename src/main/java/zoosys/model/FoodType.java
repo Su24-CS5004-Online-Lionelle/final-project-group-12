bb13fa0e94704052417c108fadaafb9605e1a333
YangChengLuo <156385953+YangChengLuo@users.noreply.github.com>
2024-08-12 12:42:20 -0700

    Update FoodType.java

--- src/main/java/zoosys/model/FoodType.java
+++ src/main/java/zoosys/model/FoodType.java
@@ -1,5 +1,11 @@
 package zoosys.model;
 
+/**
+ * Enum representing different food type for animal.
+ */
 public enum FoodType {
+    /**
+     * Represents animals that eats vegetable or meat.
+     */
     MEAT, VEGETABLE;
 }
