d628c82de47e7926109aa6505ce1b09d0405e049
YangChengLuo <156385953+YangChengLuo@users.noreply.github.com>
2024-08-12 13:20:46 -0700

    Update Visitor.java

--- src/main/java/zoosys/model/Visitor.java
+++ src/main/java/zoosys/model/Visitor.java
@@ -11,11 +11,21 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-
+/**
+ * Yangcheng Luo
+ * Class implementing the IVisitor interface.
+ * Make sure all methods in the IVisitor interface can be properly used.
+ */
 public class Visitor implements IVisitor {
+
+    // List to store visit records
     private List<Visit> visits;
+    // Map to store pricing information based on visitor categories
     private Map<String, Double> pricing;
 
+    /**
+     * Constructor initializing the visits list and setting default pricing for different categories.
+     */
     public Visitor() {
         this.visits = new ArrayList<>();
         this.pricing = new HashMap<>();
@@ -26,30 +36,48 @@ public class Visitor implements IVisitor {
         pricing.put("minor", 5.0);
     }
 
+    /**
+     * Adds a new visit.
+     *
+     * @param date                 The date of the visit in the format "MM-DD-YYYY".
+     * @param entryTime            The entry time of the visit in hours (24-hour format,for example 16 is 4 pm).
+     * @param category             The age group of the visit.
+     * @param duration             The time(hours) visitor stays.
+     * @param animalFeedback       The feedback rating for animals.
+     * @param cleanlinessFeedback  The feedback rating for cleanliness.
+     * @param pricingFeedback      The feedback rating for pricing.
+     */
     @Override
     public void addVisit(String date, int entryTime, String category, int duration, Integer animalFeedback, Integer cleanlinessFeedback, Integer pricingFeedback) {
         Visit visit = new Visit(date, entryTime, category, duration, animalFeedback, cleanlinessFeedback, pricingFeedback);
         visits.add(visit);
     }
 
+    /**
+     * Retrieves a list of all visit records.
+     *
+     * @return A list of Visit objects.
+     */
     @Override
     public List<Visit> getVisits() {
         return new ArrayList<>(visits);
     }
-    @Override
-    public Visit getVisit(int index) {
-        if (index >= 0 && index < visits.size()) {
-            return visits.get(index);
-        } else {
-            throw new IndexOutOfBoundsException("Invalid visit index");
-        }
-    }
 
+    /**
+     * Retrieves the total number of visits recorded in the system.
+     *
+     * @return The total number of visits.
+     */
     @Override
     public int getTotalVisits() {
         return visits.size();
     }
 
+     /**
+     * Retrieves the total number of visits.
+     *
+     * @return The total number of visits.
+     */
     @Override
     public int getVisitsCountByDate(String date) {
         int count = 0;
@@ -61,6 +89,12 @@ public class Visitor implements IVisitor {
         return count;
     }
 
+    /**
+     * Find out the peak hour for zoo.
+     *
+     * @param date The date of interest in the format "MM-DD-YYYY".
+     * @return The hour of the day (0-23) with the most entries.
+     */
     @Override
     public int getPeakHourByDate(String date) {
         Map<Integer, Integer> hourCount = new HashMap<>();
@@ -80,6 +114,12 @@ public class Visitor implements IVisitor {
         return peakHour;
     }
 
+    /**
+     * The average visitor feedback for animals by date.
+     *
+     * @param date The date of interest in the format "MM-DD-YYYY".
+     * @return The average animal feedback (1-5).
+     */
     @Override
     public double getAverageAnimalFeedbackByDate(String date) {
         int totalFeedback = 0;
@@ -93,6 +133,12 @@ public class Visitor implements IVisitor {
         return count == 0 ? 0 : (double) totalFeedback / count;
     }
 
+    /**
+     * The average visitor feedback for cleanliness by date.
+     *
+     * @param date The date of interest in the format "MM-DD-YYYY".
+     * @return The cleanliness animal feedback (1-5).
+     */
     @Override
     public double getAverageCleanlinessFeedbackByDate(String date) {
         int totalFeedback = 0;
@@ -105,7 +151,13 @@ public class Visitor implements IVisitor {
         }
         return count == 0 ? 0 : (double) totalFeedback / count;
     }
-
+    
+    /**
+     * The average visitor feedback for pricing by date.
+     *
+     * @param date The date of interest in the format "MM-DD-YYYY".
+     * @return The cleanliness pricing feedback (1-5).
+     */
     @Override
     public double getAveragePricingFeedbackByDate(String date) {
         int totalFeedback = 0;
@@ -119,11 +171,22 @@ public class Visitor implements IVisitor {
         return count == 0 ? 0 : (double) totalFeedback / count;
     }
 
+    /**
+     * Sets the pricing for different age group.
+     *
+     * @param pricing A map where the key is the visit category and the value is the corresponding price.
+     */
+
     @Override
     public void setPricing(Map<String, Double> pricing) {
         this.pricing = pricing;
     }
 
+    /**
+     * Sets the pricing for different age group.
+     *
+     * @param pricing A map where the key is the visit category and the value is the corresponding price.
+     */
     @Override
     public double getRevenueByDate(String date) {
         double totalRevenue = 0;
@@ -138,6 +201,11 @@ public class Visitor implements IVisitor {
         return totalRevenue;
     }
 
+    /**
+     * Provides a string representation of the Visitor object.
+     *
+     * @return A string representation of the visitor object, including visits and pricing.
+     */
     @Override
     public String toString() {
         return "VisitorImpl{" +
@@ -146,6 +214,10 @@ public class Visitor implements IVisitor {
                 '}';
     }
 
+    /**
+     * Reads visit data from a CSV file and populates the system with this data.
+     * Each line in the file represents a visit, which is parsed and added to the system.
+     */
     @Override
     public void readCSV() {
         List<String> lines;
